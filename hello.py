270253c4449249ed96a64edc7bcd1db9dfd3410e
kimkh1517@gmail.com <kimkh1517@gmail.com>
2022-01-21 21:33:11 +0900

    module, zerodivisionError

---
+++ hello.py
@@ -0,0 +1,11 @@
+def say_name(name:str):
+    print("hello my name is {}".format(name))
+    
+def add(a, b):
+    return a + b
+
+def minus(a, b):
+    if a >= b:
+        return a - b
+    else: 
+        return b - a
